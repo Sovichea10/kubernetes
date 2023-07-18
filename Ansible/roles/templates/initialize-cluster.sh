6c38399b21eca87765a900af93d03622325c0edb
sovichea <vicheasokan6@gmail.com>
2023-07-18 14:41:42 +0700

    update 18/07/2023

---
+++ Ansible/roles/templates/initialize-cluster.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+IPADDR=$(curl ifconfig.me && echo "")
+NODENAME=$(hostname -s)
+POD_CIDR="192.168.0.0/16"
+
+sudo kubeadm init --control-plane-endpoint=$IPADDR  --apiserver-cert-extra-sans=$IPADDR  --pod-network-cidr=$POD_CIDR --node-name $NODENAME --ignore-preflight-errors Swap
